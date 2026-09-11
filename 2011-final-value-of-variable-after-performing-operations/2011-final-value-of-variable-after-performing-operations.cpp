e3b97c5271010123eaa3093424aa1a89e726b96e
A_Sin_code <aditya208146@gmail.com>
2026-09-11 23:29:08 +0530

    Time: 1 ms (14.56%), Space: 17.7 MB (9.86%) - LeetHub

---
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) {
+        
+        
+        int x = 0;
+        for(auto s : operations)
+            x += (s[1] == '+') ? 1 : -1;
+        return x;
+    
+    }
+};
